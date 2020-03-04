eab5162a20ca2c467320030e5da063c80cf5a8f0
Zaitian Mou <thomasmou@outlook.com>
2020-03-03 20:27:43 -0500

    Build.

---
+++ precache-manifest.8c91b7824178e08578fcdf956ed37d90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5c645f1810a5d7355da9551e6a296d",
+    "url": "/Wireframer/index.html"
+  },
+  {
+    "revision": "d27b5113a65df4570843",
+    "url": "/Wireframer/static/css/main.838438de.chunk.css"
+  },
+  {
+    "revision": "9c1b3707077c77553931",
+    "url": "/Wireframer/static/js/2.e5435536.chunk.js"
+  },
+  {
+    "revision": "d27b5113a65df4570843",
+    "url": "/Wireframer/static/js/main.0f9e3034.chunk.js"
+  },
+  {
+    "revision": "e16a737dfb0e937537a2",
+    "url": "/Wireframer/static/js/runtime-main.4212764a.js"
+  }
+]);
